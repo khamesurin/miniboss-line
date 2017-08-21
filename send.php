fa8c5d7ec71771ec348f36587008028ba8dc07ac
khamesurin <numkangg12@gmail.com>
2017-08-22 02:23:36 +0700

    saaa

--- send.php
+++ send.php
@@ -34,7 +34,7 @@ $set_price = $_GET['set_price'];
 $last_price = $_GET['last_price'];
 $txt_line = ''.$name.' , Price more than '.$set_price.' To '.$last_price.' ';			
 			$post = '{
-    "to": "U493a81bb89e58db5f619db64a94f7d08",
+    "to": ["U493a81bb89e58db5f619db64a94f7d08","U331603b5a1d3ea48b78e7eb00364e196"],
     "messages":[
         {
             "type":"text",
@@ -51,7 +51,7 @@ $txt_line = ''.$name.' , Price more than '.$set_price.' To '.$last_price.' ';
 }*/
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			if(0 > 1){
+			if(2 > 1){
 				
 			
 			$ch = curl_init($url);
