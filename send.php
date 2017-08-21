994b6e4261dc7b4e1f46b9c8ca8f3f01417280bc
khamesurin <numkangg12@gmail.com>
2017-08-22 01:37:37 +0700

    ff

--- send.php
+++ send.php
@@ -1,7 +1,33 @@
 <?php
 $access_token = 'g71f3DlMvXejMozh2QiWtpOWp0FFUvTWfQv+VGnSn0mvj9EpSCFXNakeWFM4XgkpyW3OGyHJ3DXHwuql0FMcaXXdtrphoWkCC+oV+5MpTb7VnA8sDcq2XlI2KHTdJVde4fuacAnvdSBFBcxOLzJ5CgdB04t89/1O/w1cDnyilFU=';
 
-$name = $_GET['name'];
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+//if (!is_null($events['events'])) {
+	// Loop through each event
+	//foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		//if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data = [
+				'to' => "khamenaja",
+				'messages' => "test"
+			];
 			$post = '{
     "to": "U493a81bb89e58db5f619db64a94f7d08",
     "messages":[
@@ -28,5 +54,7 @@ $name = $_GET['name'];
 			curl_close($ch);
 
 			echo $result . "\r\n";
-
+		//}
+	//}
+//}
 echo "OK";
