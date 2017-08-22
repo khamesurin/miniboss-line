fad22e4a20b7a12ff48b519bb40beff8c889da91
khamesurin <numkangg12@gmail.com>
2017-08-22 11:06:56 +0700

    updated

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text.$result.$urlLine
+				'text' => $result
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
