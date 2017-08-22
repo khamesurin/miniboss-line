c0c46de7ec3bb8068ec274ac61568433e554fcc4
khamesurin <numkangg12@gmail.com>
2017-08-22 12:48:24 +0700

    dddd

--- send-multi.php
+++ send-multi.php
@@ -35,7 +35,7 @@ $set_price = $_GET['set_price'];
 $last_price = $_GET['last_price'];
 $txt_line = ''.$name.' , Price more than '.$set_price.' To '.$last_price.' ';			
 			$post = '{
-    "to": ["U493a81bb89e58db5f619db64a94f7d08","U97a49f76c0043e0b3d4f81121248cf16","U4155056f90bb4eb8f2bc08fbf0c200d1"],
+    "to": ["U493a81bb89e58db5f619db64a94f7d08","U97a49f76c0043e0b3d4f81121248cf16","U4155056f90bb4eb8f2bc08fbf0c200d1","U331603b5a1d3ea48b78e7eb00364e196"],
     "messages":[
         {
             "type":"text",
