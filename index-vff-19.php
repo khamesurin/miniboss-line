fb03fe78e4b0ff8ab4d6ef0cff6c8de6364dcb3e
khamesurin <numkangg12@gmail.com>
2017-08-22 12:37:19 +0700

    ssssss

--- index-vff-19.php
+++ index-vff-19.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			$mid = $event['message']['mid'];
+			$mid = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
